8a17db22f977f532196fedbc11c5f7965b611d1c
ja699461 <73557462+ja699461@users.noreply.github.com>
2021-12-15 14:30:54 -0500

    Update parse_course.php

--- parse_course.php
+++ parse_course.php
@@ -1,50 +1,56 @@
 <?php
-$parse = ;
+
+$parse=$_POST['parse'];
+
+	$a = explode("\n",$parse);
+	$b = explode("| ",$a[0]);
+	$d = explode("  ",$a[2]);
+	$e = explode("Type: ",$a[10]);
+	$f = explode("Room:",$a[10]);
+	$ff = explode("\n",$f[1]);
+	$g = explode("Building: ",$f[0]);
+	$h = explode("  ",$a[11]);
+	$i = explode("Type: ",$a[19]);
+	$j = explode("Room:",$a[19]);
+	$jj = explode("\n",$j[1]);
+	$k = explode("Building: ",$j[0]);
+	$l = explode("Instructor: ",$parse);
+	$ll = explode("(Primary)",$l[1]);
+	$m = explode("CRN: ",$parse);
+
+	$coursename = $b[0];
+	$courseinfo = $b[1];
+	$day1 = $d[1];
+	$room1 = $ff[0];
+	$time1 = $e[0];
+	$location1 = $g[1];
+	$prof = $ll[0];
+	$crn = $m[1];
+	$day2 = $h[0];
+	$room2 = $jj[0];
+	$time2 = $i[0];
+	$location2 = $k[1];
+	
+	if ($room2==NULL){
+		$day2=NULL;
+	    }
+
+	echo $coursename;
+	echo $coursename;	
+	echo $courseinfo;
+	echo $day1;
+	echo $room1;
+	echo $time1;
+	echo $location1;
+	echo $prof;
+	echo $crn;
+	echo $day2;
+	echo $room2;
+	echo $time2;
+	echo $location2;
+
+ 
 
 
-$a = explode("\n",$parse);
-$b = explode("| ",$a[0]);
-$d = explode("  ",$a[2]);
-$e = explode("Type: ",$a[10]);
-$f = explode("Room:",$a[10]);
-$ff = explode("\n",$f[1]);
-$g = explode("Building: ",$f[0]);
-$h = explode("  ",$a[11]);
-$i = explode("Type: ",$a[19]);
-$j = explode("Room:",$a[19]);
-$jj = explode("\n",$j[1]);
-$k = explode("Building: ",$j[0]);
-$l = explode("Instructor: ",$parse);
-$ll = explode("(Primary)",$l[1]);
-$m = explode("CRN: ",$parse);
+ ?>
 
-$coursename = $b[0];
-$courseinfo = $b[1];
-$day1 = $d[1];
-$room1 = $ff[0];
-$time1 = $e[0];
-$location1 = $g[1];
-$prof = $ll[0];
-$crn = $m[1];
-$day2 = $h[0];
-$room2 = $jj[0];
-$time2 = $i[0];
-$location2 = $k[1];
-if ($room2==NULL){
-	$day2=NULL;
-    }
-
-echo $coursename;
-echo $coursename;
-echo $courseinfo;
-echo $day1;
-echo $room1;
-echo $time1;
-echo $location1;
-echo $prof;
-echo $crn;
-echo $day2;
-echo $room2;
-echo $time2;
-echo $location2;
-?>
